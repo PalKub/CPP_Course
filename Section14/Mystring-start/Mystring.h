0382a547e6611ecd0b909c687fc42b525ecc1745
Jakub Paluch <paluchk@gmail.com>
2025-09-12 00:15:25 +0200

    Added Section 14 with the first lesson.

---
+++ Section14/Mystring-start/Mystring.h
@@ -0,0 +1,19 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring
+{
+private:
+    char *str;  //pointre to a char[] that holds a C-style string
+public:
+    Mystring();                           //No-args constructor
+    Mystring(const char*s);               //Overloaded constructor
+    Mystring(const Mystring &source);     //Copy constructor
+    ~Mystring();                          //Destructor
+    void display() const;
+    int get_length() const;               //getters
+    const char *get_str() const;
+
+};
+
+#endif // _MYSTRING_H_
