a4df4ca329b8e9aecba603006a3d0ca1ad53c837
Jakub Paluch <paluchk@gmail.com>
2025-09-15 19:23:53 +0200

    Added Mystring-move-assignment lesson to section 14.

---
+++ Section14/Mystring-move-assignment/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include "Mystring.h"
+
+using namespace std;
+
+int main() {
+    Mystring a {"Hello"};          //Overloaded constructor
+    a = Mystring{"Hola"};          //Overloaded constructor then move assignment
+    a = "Bonjour";                 //Overloaded constructor then move assignment
+
+
+    Mystring empty;                //no-args constructor
+    Mystring larry("Larry");       //overloaded constructor
+    Mystring stooge {larry};       //copy constructor
+    Mystring stooges;              //no-args constructor
+    
+    empty = stooge;                //copy assignment operator
+                                   //stoog is an l-value
+    
+    empty = "Funny";               //move assignment operator
+                                   //"Funny" is an r=value
+    
+    empty.display();               //Larry : 5
+    larry.display();               //Larry : 5
+    stooge.display();              //Larry : 5
+    empty.display();               //Larry : 5
+    
+    stooges = "Larry, Moe and Curly";
+    stooges.display();             //Larry, Moe and Curly : 20
+    
+    vector<Mystring> stooges_vec;
+    stooges_vec.push_back("Larry");
+    stooges_vec.push_back("Moe");
+    stooges_vec.push_back("Curly");
+    
+    cout << "=== Loop 1 =======================" << endl;
+    for (const Mystring &s: stooges_vec)
+        s.display();               //Larry
+                                   //Moe
+                                   //Curly
+    
+    cout << "=== Loop 2 =======================" << endl;
+    for (Mystring &s: stooges_vec)
+        s = "Changed";               //copy assignment
+
+    
+    cout << "=== Loop 3 =======================" << endl;
+    for (const Mystring &s: stooges_vec)
+        s.display();               //Changed
+                                   //Changed
+                                   //Changed
+                                   
+    return 0;
+}
