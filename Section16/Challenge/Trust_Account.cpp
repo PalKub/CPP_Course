8d83869ebedec868d6f077384dc67f129207fb82
Jakub Paluch <paluchk@gmail.com>
2025-09-26 17:17:31 +0200

    Added a Challenge exercise to Section 16.

---
+++ Section16/Challenge/Trust_Account.cpp
@@ -0,0 +1,26 @@
+#include "Trust_Account.h"
+
+Trust_Account::Trust_Account(std::string name, double balance, double int_rate)
+    : Savings_Account{name, balance, int_rate}, withdraw_times_left{max_withdrawal_times} {
+}
+
+bool Trust_Account::deposit(double amount) {
+    if(amount >= deposit_bonus_threshold)
+        return Savings_Account::deposit(amount + deposit_bonus);
+    else
+        return  Savings_Account::deposit(amount);
+}
+
+bool Trust_Account::withdraw(double amount) {
+    if(withdraw_times_left > 0 && amount < (balance * withdraw_size_limit)) {
+        --withdraw_times_left;
+        return Savings_Account::withdraw(amount);
+    } else
+        return false;
+}
+
+void Trust_Account::print(std::ostream &os) const {
+    os.precision(2);
+    os << std::fixed;
+    os << "[Trust_Account: " << name << ": " << balance << ", " << int_rate << "%, " << deposit_bonus << "$, " << deposit_bonus_threshold << "$, " << withdraw_times_left << ", " << withdraw_size_limit << "]";
+}
