b8cfb834a68520a3d9ae7a04a0c0fee3337ba3c6
Jakub Paluch <paluchk@gmail.com>
2025-10-02 14:13:43 +0200

    Added ReadFile_3 and ReadFile_4 exercises to Section 19.

---
+++ Section19/ReadFile_3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ifstream in_file;
+    in_file.open("../ReadFile_3/poem.txt");
+    if(!in_file) {
+        std::cerr << "Problem opening file" << std::endl;
+        return 1;
+    }
+    std::string line;
+    while (std::getline(in_file, line)) {
+        std::cout << line << std::endl;
+    }
+    in_file.close();
+    return 0;
+}
