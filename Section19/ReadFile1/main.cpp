8094399d42df10c56b83ca93f0fcd93f35407c9a
Jakub Paluch <paluchk@gmail.com>
2025-10-02 14:02:40 +0200

    Added ReadFile_1 and ReadFIle_2 examples to Section 19.

---
+++ Section19/ReadFile1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ifstream in_file;
+    std::string line;
+    int num;
+    double total;
+    
+    in_file.open("../ReadFile1/test.txt");
+    if(!in_file) {
+        std::cerr << "Problem opening file" << std::endl;
+        return 1;
+    }
+    in_file >> line >> num >> total;
+    std::cout << line << std::endl;
+    std::cout << num << std::endl;
+    std::cout << total << std::endl;
+    in_file.close();
+    return 0;
+}
